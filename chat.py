c349613331e7137e7de5e2b52bd855de2267aa2e
harinarayansrivatsan <hari.narayan@email.tamu.edu>
2025-01-02 22:45:51 -0600

    Refactored code to remove redundancies

--- chat.py
+++ chat.py
@@ -10,34 +10,31 @@ load_dotenv()
 def render_chat_subheader(student_name):
     cols = st.columns([2, 4, 2], gap="small")
     with cols[1]:
-        st.subheader(f"Chat session with {student_name}", anchor=False)
-
-def render_sidebar_header():
-    cols = st.sidebar.columns([3, 5])
-    with cols[1]:
-        st.sidebar.header("""You may also ask:""")
+        st.subheader(body=f"Chat session with {student_name}", anchor=False)
 
 def render_chat_history(chat_history):
     for message in chat_history:
         with st.chat_message(name=message["role"], avatar=message["avatar"]):
             st.markdown(message["content"])
 
-def display_predefined_questions(current_chat_session, student_name):
+def display_predefined_questions(current_chat_session, student_name, student_avatar):
     if "next_questions" not in current_chat_session or current_chat_session.get("refresh_questions"):
-        chat_history = current_chat_session.get("chat_history", [])
+        chat_history = current_chat_session["chat_history"]
         if len(current_chat_session["chat_history"])>1:
-            generated_questions = asyncio.run(generate_questions_with_openai(chat_history, num_questions=4))
+            generated_questions = asyncio.run(generate_questions_with_openai(chat_history=chat_history, num_questions=4))
             current_chat_session["next_questions"] = generated_questions
         current_chat_session["refresh_questions"] = False
 
+    with st.sidebar:
+        st.markdown("---")
+        cols = st.columns([0.5, 3, 0.5])
+        with cols[1]:
+            st.title("""You may also ask\n""")
+            st.write("")
+
     for question in current_chat_session["next_questions"]:
         if st.sidebar.button(question, icon=":material/forum:", use_container_width=True):
-            handle_predefined_question(current_chat_session, question, student_name)
-
-def handle_sidebar_button(label, action, icon, current_chat_session, type):
-    if st.sidebar.button(label=label, icon=icon, use_container_width=True, type=type):
-        current_chat_session["confirm_end_chat"] = action
-        st.rerun()
+            handle_user_input(user_input=question, current_chat_session=current_chat_session, student_name=student_name, student_avatar=student_avatar)
 
 def render_sidebar_buttons(current_chat_session, buttons_config):
     for button in buttons_config:
@@ -46,14 +43,11 @@ def render_sidebar_buttons(current_chat_session, buttons_config):
             end_chat_session(chat_session_id=current_chat_session["chat_session_id"])
             st.rerun()
 
-def handle_predefined_question(current_chat_session, question, student_name):
-    handle_user_input(user_input=question, current_chat_session=current_chat_session, student_name=student_name)
-
-def handle_user_input(user_input, current_chat_session, student_name):
+def handle_user_input(user_input, current_chat_session, student_name, student_avatar):
     add_message_to_history(current_chat_session=current_chat_session, role="user", content=user_input, avatar="user")
     current_chat_session["refresh_questions"] = True
     response = get_api_response_with_spinner(user_input, current_chat_session, student_name)
-    add_message_to_history(current_chat_session=current_chat_session, role="assistant", content=response.get('answer', 'Unexpected error. Please contact support'), avatar=current_chat_session["selected_student"]["image"])
+    add_message_to_history(current_chat_session=current_chat_session, role="assistant", content=response.get('answer', 'Unexpected error. Please contact support'), avatar=student_avatar)
 
 def add_message_to_history(current_chat_session, role, content, avatar):
     if role == "user":
@@ -76,27 +70,21 @@ def load_chat_page():
     current_chat_session = st.session_state["login_sessions"]["chat_sessions"][-1]
     student_name = current_chat_session["selected_student"]["name"]
     student_avatar = current_chat_session["selected_student"]["image"]
-
-    with st.sidebar:
-        st.write("")
+    chat_history = current_chat_session["chat_history"]
+    sidebar_buttons_config = [{"label": "Back to Main Page", "action": "main", "icon": ":material/arrow_back:", "type": "primary"}, {"label": "Start new session", "action": "choice", "icon": ":material/arrow_outward:", "type": "secondary"}]
 
     render_chat_subheader(student_name)
 
     if current_chat_session["confirm_end_chat"]:
+        st.sidebar.write("")
         handle_end_chat_confirmation(current_chat_session=current_chat_session)
         return
     
-    sidebar_buttons_config = [
-        {"label": "Back to Main Page", "action": "main", "icon": ":material/arrow_back:", "type": "primary"},
-        {"label": "Start new session", "action": "choice", "icon": ":material/arrow_outward:", "type": "secondary"}
-    ]
-    render_sidebar_buttons(current_chat_session, sidebar_buttons_config)
+    render_sidebar_buttons(current_chat_session=current_chat_session, buttons_config=sidebar_buttons_config)
 
-    render_sidebar_header()
-
-    render_chat_history(current_chat_session["chat_history"])
-    user_input = st.chat_input("Enter your message")
-    display_predefined_questions(current_chat_session, student_name)
+    render_chat_history(chat_history=chat_history)
+    user_input = st.chat_input(placeholder="Enter your message")
+    display_predefined_questions(current_chat_session=current_chat_session, student_name=student_name, student_avatar=student_avatar)
 
     if user_input:
-        handle_user_input(user_input, current_chat_session, student_name)
+        handle_user_input(user_input=user_input, current_chat_session=current_chat_session, student_name=student_name, student_avatar=student_avatar)
