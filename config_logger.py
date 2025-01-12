f21b8b0e691f5d7c9543a06f51b774acb7b57281
harinarayansrivatsan <hari.narayan@email.tamu.edu>
2025-01-11 18:23:48 -0600

    Modified logging configuration

--- config_logger.py
+++ config_logger.py
@@ -1,24 +1,23 @@
-import logging
 import os
 
-from datetime import datetime
 from dotenv import load_dotenv
+from logging import getLogger, Formatter, INFO, TimedRotatingFileHandler
 
 load_dotenv()
 
-log_dir = os.path.abspath(os.getenv("LOG_FILE_PATH"))
-os.makedirs(log_dir, exist_ok=True)  
+log_dir = os.path.abspath(os.getenv("LOG_FOLDER_PATH", default="logs"))
+os.makedirs(name=log_dir, exist_ok=True)
 
-timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-log_file = os.path.join(log_dir, f"application_logs.log")
-agastya_logger = logging.getLogger("agastya_logger")
-agastya_logger.setLevel(logging.INFO)
+log_file = os.path.join(log_dir, "application_logs.log")
+
+agastya_logger = getLogger(name="agastya_logger")
+agastya_logger.setLevel(level=INFO)
 
 if not agastya_logger.handlers:
-    file_handler = logging.FileHandler(log_file)
-    file_handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    file_handler.setFormatter(formatter)
-    agastya_logger.addHandler(file_handler)
+    file_handler = TimedRotatingFileHandler(filename=log_file, when="midnight", interval=1, utc=True)
+    file_handler.setLevel(INFO)
+    formatter = Formatter(fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(fmt=formatter)
+    agastya_logger.addHandler(hdlr=file_handler)
 
 agastya_logger.propagate = False
