94fb26ae85a2d57b6fca240499693b5782b2bdd5
harinarayansrivatsan <hari.narayan@email.tamu.edu>
2025-01-13 20:54:42 -0600

    Removed excess elements from UI

--- choice.py
+++ choice.py
@@ -15,10 +15,7 @@ async def load_choice_page():
 
         if st.button(label="Back to Main Page", icon=":material/arrow_back:", type="primary", use_container_width=True):
             switch_page("main")
-            
-    st.markdown("<div style='text-align: center; font-size: 4rem;'><strong>Pick a student to interact with</strong></div>", unsafe_allow_html=True)
-    st.markdown("---")
-    
+
     try:
         with open("students.json", "r") as file:
             students = json.load(file)
@@ -32,9 +29,10 @@ async def load_choice_page():
         cols = st.columns(spec=[5, 1, 5, 1, 5, 1, 5, 1], gap="large")
         for idx, student in enumerate(row):
             with cols[2*idx]:
-                st.markdown(f"<div style='text-align: center; font-size: 1.5rem;'><strong>{student['name']}</strong></div>", unsafe_allow_html=True)
-                st.image(student["image"], use_container_width=True)
-                if st.button(f"Chat with {student['name']}", key=f"chat_with_{student['name']}", icon=":material/arrow_outward:", type="primary", use_container_width=True):
+                st.markdown(body=f"<div style='text-align: center; font-size: 1.5rem;'><strong>{student['name']}</strong></div>", unsafe_allow_html=True)
+                st.write(" ")
+                st.image(image=student["image"], use_container_width=True)
+                if st.button(label=f"Chat with {student['name']}", key=f"chat_with_{student['name']}", icon=":material/arrow_outward:", type="primary", use_container_width=True):
                     progress_text=f"Loading chat session with {student['name']}..."
                     progress_bar = st.progress(0, text=f"{progress_text} ({0}%)")
                     await initialize_chat_session(student_profile=student)
@@ -49,7 +47,5 @@ async def load_choice_page():
                     st.write(f"**Sex:** {student['sex']}")
                     st.write(f"**Region:** {student['region']}")
         
-        if row == rows[-1]:
-            st.markdown(" ")
-        else:
-            st.markdown("---")
+        with st.empty():
+            st.markdown("--- \n")
