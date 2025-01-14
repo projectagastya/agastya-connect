b5f203d8ededcd38e2e6d109e8b435eba5c757f7
harinarayansrivatsan <hari.narayan@email.tamu.edu>
2025-01-13 18:41:18 -0600

    Changed layout to fit 4 students in a row; Added custom CSS

--- choice.py
+++ choice.py
@@ -16,12 +16,9 @@ async def load_choice_page():
         if st.button(label="Back to Main Page", icon=":material/arrow_back:", type="primary", use_container_width=True):
             switch_page("main")
             
-    cols = st.columns(spec=[1,5,1], gap="small")
-    
-    with cols[1]:
-        st.title("Pick a student to interact with", anchor=False)
-        st.write("")
+    st.markdown("<div style='text-align: center; font-size: 4rem;'><strong>Pick a student to interact with</strong></div>", unsafe_allow_html=True)
     st.markdown("---")
+    
     try:
         with open("students.json", "r") as file:
             students = json.load(file)
@@ -29,15 +26,15 @@ async def load_choice_page():
         st.error("Students data file not found.")
         return
 
-    rows = [students[i:i + 2] for i in range(0, len(students), 2)]
+    rows = [students[i:i + 4] for i in range(0, len(students), 4)]
 
     for row in rows:
-        cols = st.columns([7.5, 5, 1, 0.8, 7.5, 5, 1])
+        cols = st.columns(spec=[8, 8, 8, 8], gap="large")
         for idx, student in enumerate(row):
-            offset = idx * 4
-            with cols[offset]:
+            with cols[idx]:
+                st.markdown(f"<div style='text-align: center; font-size: 2rem;'><strong>{student['name']}</strong></div>", unsafe_allow_html=True)
                 st.image(student["image"], use_container_width=True)
-                if st.button(f"Chat with {student['name']}", key=student['name'], icon=":material/arrow_outward:", type="primary", use_container_width=True):
+                if st.button(f"Chat with {student['name']}", key=f"chat_with_{student['name']}", icon=":material/arrow_outward:", type="primary", use_container_width=True):
                     progress_text=f"Loading chat session with {student['name']}..."
                     progress_bar = st.progress(0, text=f"{progress_text} ({0}%)")
                     await initialize_chat_session(student_profile=student)
@@ -53,6 +50,5 @@ async def load_choice_page():
                     st.write(f"**Age:** {student['age']}")
                     st.write(f"**Sex:** {student['sex']}")
                     st.write(f"**Region:** {student['region']}")
-            
-            if idx % 2 == 0:
-                st.markdown("---")
+        
+        st.markdown("---")
