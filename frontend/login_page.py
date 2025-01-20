3ecce68649ab97a557d56e913ba817a9c61ab301
harinarayansrivatsan <hari.narayan@email.tamu.edu>
2025-01-20 14:12:06 -0600

    Wrapped elements in containers

--- frontend/login_page.py
+++ frontend/login_page.py
@@ -9,43 +9,45 @@ from frontend.all_utils import add_aligned_text, generate_uuid, switch_page, val
 load_dotenv()
 
 async def load_login_page():
-    with st.container():
+    with st.container(border=False):
         col1, col2 = st.columns(spec=[1.0, 0.5], gap="large")
 
         with col1:
-            add_aligned_text(content="Welcome to Agastya International Foundation", size=39, alignment="center", bold=True)
-            add_aligned_text(content="The world's largest \"creativity laboratory\"", size=28, alignment="center", bold=True, italics=True)
-            st.markdown(body=" ", unsafe_allow_html=True)
-            add_aligned_text(content="Our vision is a creative world of tinkerers, innovators and solution-seekers who are humane, anchored and connected. Our mission is to spark curiosity (Aah!), nurture creativity (Aha!), and instill confidence and caring (Ha-Ha!) in children and teachers - through transformative experiential learning.", size=18, alignment="justify")
-            st.markdown(body="<br>", unsafe_allow_html=True)
-            with st.container(height=500, border=False):
-                st.image(image="./frontend/images/main.png", use_container_width=True)
+            with st.container(border=False):
+                add_aligned_text(content="Welcome to Agastya International Foundation", size=39, alignment="center", bold=True)
+                add_aligned_text(content="The world's largest \"creativity laboratory\"", size=28, alignment="center", bold=True, italics=True)
+                st.markdown(body=" ", unsafe_allow_html=True)
+                add_aligned_text(content="Our vision is a creative world of tinkerers, innovators and solution-seekers who are humane, anchored and connected. Our mission is to spark curiosity (Aah!), nurture creativity (Aha!), and instill confidence and caring (Ha-Ha!) in children and teachers - through transformative experiential learning.", size=18, alignment="justify")
+                st.markdown(body="<br>", unsafe_allow_html=True)
+                with st.container(height=500, border=False):
+                    st.image(image="./frontend/images/main.png", use_container_width=True)
 
         with col2:
-            add_aligned_text(content="User Login", size=39, alignment="center", bold=True)
-            username = st.text_input(label="Username", placeholder="Enter your username", type="default")
-            password = st.text_input(label="Password", placeholder="Enter your password", type="password")
-            st.markdown(body=" ", unsafe_allow_html=True)
-            subcols = st.columns(spec=[1, 1], gap="small")
-            with subcols[0]:
-                if st.button(label="Login", icon= ":material/login:", type="primary", use_container_width=True):
-                    if validate_credentials(username=username, password=password):
-                        st.session_state["username"] = username
-                        st.session_state["login_sessions"] = {
-                            "id": generate_uuid(),
-                            "login_timestamp": datetime.now(tz=pytz.timezone(zone=os.getenv(key="TIMEZONE", default="US/Central"))),
-                            "logout_timestamp": None,
-                            "chat_sessions": [],
-                            "active_chat_session": None
-                        }
-                        switch_page(page_name="main")
-                    else:
-                        st.error(body="Invalid credentials.")
-            with subcols[1]:
-                if st.button(label="Forgot Password ?", icon=":material/lock_reset:", use_container_width=True):
+            with st.container(border=False):
+                add_aligned_text(content="User Login", size=39, alignment="center", bold=True)
+                username = st.text_input(label="Username", placeholder="Enter your username", type="default")
+                password = st.text_input(label="Password", placeholder="Enter your password", type="password")
+                st.markdown(body=" ", unsafe_allow_html=True)
+                subcols = st.columns(spec=[1, 1], gap="small")
+                with subcols[0]:
+                    if st.button(label="Login", icon= ":material/login:", type="primary", use_container_width=True):
+                        if validate_credentials(username=username, password=password):
+                            st.session_state["username"] = username
+                            st.session_state["login_sessions"] = {
+                                "id": generate_uuid(),
+                                "login_timestamp": datetime.now(tz=pytz.timezone(zone=os.getenv(key="TIMEZONE", default="US/Central"))),
+                                "logout_timestamp": None,
+                                "chat_sessions": [],
+                                "active_chat_session": None
+                            }
+                            switch_page(page_name="main")
+                        else:
+                            st.error(body="Invalid credentials.")
+                with subcols[1]:
+                    if st.button(label="Forgot Password ?", icon=":material/lock_reset:", use_container_width=True):
+                        switch_page(page_name="signup")
+                if st.button(label="New User ? Sign Up", icon=":material/person_add:", use_container_width=True):
                     switch_page(page_name="signup")
-            if st.button(label="New User ? Sign Up", icon=":material/person_add:", use_container_width=True):
-                switch_page(page_name="signup")
-            
-            with st.container(height=400, border=False):
-                st.image(image="frontend/images/login_pic.png", use_container_width=True)
+                
+                with st.container(height=380, border=False):
+                    st.image(image="frontend/images/login_pic.png", width=380)
