b37cf15cb6af1a07d1fb89548ab8e75ede77d256
harinarayansrivatsan <hari.narayan@email.tamu.edu>
2025-01-20 13:43:24 -0600

    Modified layout; used st.container() for images

--- frontend/login_page.py
+++ frontend/login_page.py
@@ -13,18 +13,16 @@ async def load_login_page():
         col1, col2 = st.columns(spec=[1.0, 0.5], gap="large")
 
         with col1:
-            add_aligned_text(content="Welcome to Agastya International Foundation", size=48, alignment="center", bold=True)
-            add_aligned_text(content="The world's largest \"creativity laboratory\"", size=40, alignment="center")
-            add_aligned_text(content="\"Agastya is a cause - not just an organization\"", size=24, alignment="center", bold=True, italics=True)
+            add_aligned_text(content="Welcome to Agastya International Foundation", size=39, alignment="center", bold=True)
+            add_aligned_text(content="The world's largest \"creativity laboratory\"", size=28, alignment="center", bold=True, italics=True)
+            st.markdown(body=" ", unsafe_allow_html=True)
+            add_aligned_text(content="Our vision is a creative world of tinkerers, innovators and solution-seekers who are humane, anchored and connected. Our mission is to spark curiosity (Aah!), nurture creativity (Aha!), and instill confidence and caring (Ha-Ha!) in children and teachers - through transformative experiential learning.", size=18, alignment="justify")
             st.markdown(body="<br>", unsafe_allow_html=True)
-            add_aligned_text(content="Our vision is a creative world of tinkerers, innovators and solution-seekers who are humane, anchored and connected. Our mission is to spark curiosity (Aah!), nurture creativity (Aha!), and instill confidence and caring (Ha-Ha!) in children and teachers - through transformative experiential learning.", size=24, alignment="justify")
-            st.markdown(body="<br>", unsafe_allow_html=True)
-            cols = st.columns(spec=[0.1,1,0.01], gap="small")
-            with cols[1]:
-                st.image(image="./frontend/images/main.png", width=950)
+            with st.container(height=500, border=False):
+                st.image(image="./frontend/images/main.png", use_container_width=True)
 
         with col2:
-            add_aligned_text(content="User Login", size=48, alignment="center", bold=True)
+            add_aligned_text(content="User Login", size=39, alignment="center", bold=True)
             username = st.text_input(label="Username", placeholder="Enter your username", type="default")
             password = st.text_input(label="Password", placeholder="Enter your password", type="password")
             st.markdown(body=" ", unsafe_allow_html=True)
@@ -46,7 +44,8 @@ async def load_login_page():
             with subcols[1]:
                 if st.button(label="Forgot Password ?", icon=":material/lock_reset:", use_container_width=True):
                     switch_page(page_name="signup")
-            if st.button(label="New User? Sign Up", icon=":material/assignment_ind:", use_container_width=True):
+            if st.button(label="New User ? Sign Up", icon=":material/person_add:", use_container_width=True):
                 switch_page(page_name="signup")
-            st.markdown(body="<br>", unsafe_allow_html=True)
-            st.image(image="frontend/images/login_pic.png",width=500)
+            
+            with st.container(height=400, border=False):
+                st.image(image="frontend/images/login_pic.png", use_container_width=True)
