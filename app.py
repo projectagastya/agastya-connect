412291eedb40f90ed3d96e9bd2132eac99873e43
harinarayansrivatsan <hari.narayan@email.tamu.edu>
2025-07-11 20:28:18 -0700

    Fixed app.py error

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ import streamlit as st
 
 from utils.frontend.all import authenticated, reset_session_state
 
-from utils.shared.errors import get_user_error
 def main():
     if not authenticated():
-        st.error(get_user_error())
-        st.stop()
+        st.switch_page("pages/login.py")
     else:
         reset_session_state()
         st.switch_page("pages/home.py")
